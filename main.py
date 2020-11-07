c652653e42c4eff86bd3c81cd4e584e4abb9db2c
99002487 <sriniketan.r@ltts.com>
2020-11-07 11:28:02 +0530

    srini contribution

---
+++ main.py
@@ -0,0 +1,37 @@
+
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+
+@app.route("/contact")
+def contact():
+    return render_template('contact.html')
+
+@app.route("/post")
+def post():
+    return render_template('post.html')
+
+@app.route("/post1")
+def post1():
+    return render_template('post1.html')
+
+@app.route("/post2")
+def post2():
+    return render_template('post2.html')
+
+
+@app.route("/post3")
+def post3():
+    return render_template('post3.html')
+
+app.run(debug=True)
